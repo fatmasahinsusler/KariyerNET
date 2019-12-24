d003a40f1b26144e8442734d6d7a8192ba02b7a9
Fatmasahin <56836376+Fatmasahin@users.noreply.github.com>
2019-12-24 21:56:23 +0300

    Core Katmanı tamamlandı.

---
+++ KariyerNET.Core/DAL/EntityFramework/EFSignletonDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerNET.Core.DAL.EntityFramework
+{
+    public class EFSignletonDbContext<TContext>
+         where TContext : DbContext,new()
+    {
+        private static TContext _context;
+        private EFSignletonDbContext()
+        {
+
+        }
+        public static TContext Instance
+        {
+            get
+            {
+                if (_context==null)
+                {
+                    _context = new TContext();
+                }
+                return _context;
+            }
+        }
+    }
+}
