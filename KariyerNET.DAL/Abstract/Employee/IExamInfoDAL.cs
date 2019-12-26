c324693d3d46220d9f171baad224be34d353e903
emresusler <emresusler@hotmail.com>
2019-12-26 20:53:43 +0300

    DAL - Abstract Kısım Tamamlandı

---
+++ KariyerNET.DAL/Abstract/Employee/IExamInfoDAL.cs
@@ -0,0 +1,14 @@
+﻿using KariyerNET.Core.DAL;
+using KariyerNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerNET.DAL.Abstract.Employee
+{
+    public interface IExamInfoDAL : IRepository<ExamInfo>
+    {
+    }
+}
