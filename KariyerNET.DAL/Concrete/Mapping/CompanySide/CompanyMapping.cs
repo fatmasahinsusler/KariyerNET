7c1e5f148b3ef72ca8cccb5fe8b081def740c7b6
Fatmasahin <56836376+Fatmasahin@users.noreply.github.com>
2019-12-28 18:52:59 +0300

    CompanyMapping eklendi.

--- KariyerNET.DAL/Concrete/Mapping/CompanySide/CompanyMapping.cs
+++ KariyerNET.DAL/Concrete/Mapping/CompanySide/CompanyMapping.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using KariyerNET.Model.CompanySide;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerNET.DAL.Concrete.Mapping.CompanySide
 {
-	public class CompanyMapping
+	public class CompanyMapping:EntityTypeConfiguration<Company>
 	{
+		public CompanyMapping()
+		{
+			Property(p => p.UserName).HasMaxLength(50).IsRequired();
+			Property(p => p.Password).HasMaxLength(16).IsRequired();
+			Property(p => p.CompanyName).HasMaxLength(100).IsRequired();
+			Property(p => p.LogoURL).HasMaxLength(16).IsRequired();
+			Property(p => p.Address).HasMaxLength(400).IsRequired();
+			Property(p => p.Phone).HasMaxLength(11).IsRequired();
+			Property(p => p.EMail).IsRequired();
+
+
+		}
 	}
 }
