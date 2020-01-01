a70602286d93c7873af6fc5b1782e38f8d481d14
tugbacevik <tugbacevik@users.noreply.github.com>
2020-01-01 23:57:10 +0300

    Sİngle-JobAdvertPage
    İlan, ilan başvurusu tamamlama sayfaları, actionları yarım :(

---
+++ KariyerNET.UI.MVC/Controllers/JobAdvertController.cs
@@ -0,0 +1,54 @@
+﻿using KariyerNET.BLL.Abstract.EmployeeSide;
+using KariyerNET.Model.EmployeeSide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerNET.UI.MVC.Controllers
+{
+    public class JobAdvertController : Controller
+    {
+        ILoginService _loginService;
+
+        public JobAdvertController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        // GET: JobAdvert
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(Login login)
+        //{
+
+        //    var gelenKullanici = _loginService.GetUser(login.LoginMail, login.Password);
+        //    if (gelenKullanici != null)
+        //    {
+        //        Session["user"] = gelenKullanici;
+        //        return RedirectToAction("BeforeApply","JobAdvert");
+        //    }
+        //    ViewBag.Error = "Giriş yapınız!";            
+        //    return View("Register", "Employee");
+
+
+        //}
+        public ActionResult BeforeApply(Login login)
+        {
+            var gelenKullanici = _loginService.GetUser(login.LoginMail, login.Password); 
+            if (gelenKullanici != null)
+            {
+                Session["user"] = gelenKullanici;
+                return RedirectToAction("BeforeApply", "JobAdvert");
+            }
+            ViewBag.Error = "Giriş yapınız!";
+            return RedirectToAction("Login", "Employee");
+        }
+    }
+}
